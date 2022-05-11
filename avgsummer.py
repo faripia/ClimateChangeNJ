7227b20729f0b1889809c117fe6f4bc357d508cd
faripia <96210946+faripia@users.noreply.github.com>
2022-05-11 19:43:36 -0400

    Add files via upload

---
+++ avgsummer.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# grab data
+data = pd.read_csv('njtemp.csv')
+
+#create new columns for averages
+data['Summer'] = data.iloc[:, [6,7,8,9]].mean(axis=1)
+data['Winter'] = data.iloc[:, [1,2,3]].mean(axis=1)
+
+#trendline graph
+sns.lmplot(x='Year',y='Summer',data=data, fit_reg=True) 
+plt.ylabel("TEMP")
+plt.title("NJ Average Summer Temp")
+
+#regular line graph
+data.reset_index().plot(x='Year', y=['Summer'], kind='line')
+plt.title("NJ Average Summer Temp")
+plt.xlabel("Years")
+plt.ylabel("TEMP")
+
+# Show the plot
+plt.show()
+print(data)
+
